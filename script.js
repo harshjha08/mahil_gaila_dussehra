b0ce984fd4f35a218e22d32997aacaeee662eba5
Harsh Jha <hjha1289@gmail.com>
2025-09-21 14:47:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Navratra2
+function showBox_day2(){
+    document.getElementById("day-2-role").style.display = "flex";
+}
+function closeBox_day2(){
+    document.getElementById("day-2-role").style.display = "none";
+}
+// Navratra5
+function ShowBox_day5(){
+    document.getElementById("day-5-role").style.display = "flex";
+}
+function closeBox_day5(){
+    document.getElementById("day-5-role").style.display = "none";
+}
+//navratra6
+function ShowBox_day6(){
+    document.getElementById("day-6-role").style.display = "flex";
+}
+function closeBox_day6(){
+    document.getElementById("day-6-role").style.display = "none";
+}
+//navratra8
+function ShowBox_day8(){
+    document.getElementById("day-8-role").style.display = "flex";
+}
+function closeBox_day8(){
+    document.getElementById("day-8-role").style.display = "none";
+}
